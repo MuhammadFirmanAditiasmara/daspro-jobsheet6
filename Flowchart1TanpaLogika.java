a349b1034a83fb834eb36150c896daa5ec056d13
Adidtiasmara <mfirmanadid@gmail.com>
2024-10-04 09:21:49 +0700

    Flowchart 1 Tanpa Operator Logika

---
+++ Flowchart1TanpaLogika.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Flowchart1TanpaLogika {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bil1, bil2, bil3;
+
+        System.out.print("Masukkan bilangan 1 : ");
+        bil1 = input.nextInt();
+        System.out.print("Masukkan bilangan 2 : ");
+        bil2 = input.nextInt();
+        System.out.print("Masukkan bilangan 3 : ");
+        bil3 = input.nextInt();
+
+        if (bil1 > bil2) {
+            if (bil1 > bil3) {
+                System.out.println("Bilangan terbesar adalah " + bil1);
+            }
+        } else if (bil2 > bil3) {
+            System.out.println("Bilangan terbesar adalah " + bil2);
+        } else {
+            System.out.println("Bilangan terbesar adalah " + bil3);
+            input.close();
+        }
+    }
+}
